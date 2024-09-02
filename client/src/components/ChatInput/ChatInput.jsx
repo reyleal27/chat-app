5079d4221728426999122679bf5fca9eec761648
reyleal27 <137335564+reyleal27@users.noreply.github.com>
2024-09-02 21:09:42 +0800

    Update ChatInput.jsx

--- client/src/components/ChatInput/ChatInput.jsx
+++ client/src/components/ChatInput/ChatInput.jsx
@@ -10,8 +10,7 @@ const ChatInput = ({ handleSendMsg }) => {
     const [msg, setMsg] = useState("");
     const pickerRef = useRef(null);
 
-  // const inputRef = createRef();
-  // const [cursorPosition, setCursorPosition] = useState();
+
   const handleEmojiPickerHideShow = () => {
       setShowEmojiPicker(!showEmojiPicker);
     };
@@ -48,23 +47,6 @@ useEffect(() => {
     console.log(emojiObject.native);
   };
 
-  // const handleEmojiClick = (event, { emoji }) => {
-  //     const ref = inputRef.current;
-  //     ref.current();
-  //     const start = msg.substring(0, ref.selectionStart);
-  //     const end = msg.substring(ref.selectionStart);
-  //     const text = start + emoji + end;
-  //     setMsg(text);
-  //     setCursorPosition(start.length + emoji.length);
-  //     console.log(emoji);
-  //    }
-
-  //  useEffect(() => {
-  //     if (inputRef.current) {
-  //       inputRef.current.setSelectionRange(cursorPosition, cursorPosition);
-  //     }
-  //   }, [cursorPosition]);
-
   const sendChat = (e) => {
     e.preventDefault();
     if (msg.length > 0) {
@@ -80,7 +62,6 @@ useEffect(() => {
           {showEmojiPicker && (
             <div className="emoji-wrapper" ref={pickerRef}>
               <Picker
-                //   className="emoji-picker-react"
                 data={data}
                 onEmojiSelect={handleEmojiClick}
                 previewPosition="none"
@@ -130,9 +111,9 @@ const Container = styled.div`
         color: #ffff00c8;
         cursor: pointer;
       }
-      .emoji-picker-react {
+      .emoji-wrapper {
         position: absolute;
-        top: -350px;
+        top: -420px;
         background-color: #080420;
         box-shadow: 0 5px 10px #9a86f3;
         border-color: #9a86f3;
@@ -202,74 +183,4 @@ const Container = styled.div`
     }
   }
 `;
-
-// const Container = styled.div`
-//   display: grid;
-//   grid-template-columns: 5% 95%;
-//   align-items: center;
-//   background-color: #080420;
-//   padding: 0.2rem;
-//   padding-bottom: 0.3rem;
-//   .button-container {
-//     display: flex;
-//     align-items: center;
-//     color: white;
-//     gap: 1rem;
-//     .emoji {
-//       position: relative;
-//       svg {
-//         font-size: 1.5rem;
-//         color: #ffff00c8;
-//         cursor: pointer;
-//         margin-left: 1rem;
-//       }
-//       .emoji-wrapper {
-//         position: absolute;
-//         top: -450px;
-//         background-color: #080420;
-//         box-shadow: 0 4px 7px #9a86f3;
-//         border-color: 1px solid #9a86f3;
-//       }
-//     }
-//   }
-//   .input-container {
-//     width: 100%;
-//     height: 60%;
-//     background-color: gray;
-//     border-radius: 2rem;
-//     gap: 2rem;
-//     display: flex;
-//     align-items: center;
-
-//     input {
-//       width: 90%;
-//       height: 60%;
-//       background-color: transparent;
-//       color: white;
-//       border: none;
-//       padding-left: 1rem;
-//       margin-left: 1rem;
-//       font-size: 1.2rem;
-//       &::selection {
-//         background-color: #9186f3;
-//       }
-//       &::focus {
-//         outline: none;
-//       }
-//     }
-//     button {
-//       padding: 0.3rem 2rem;
-//       border-radius: 2rem;
-//       display: flex;
-//       align-items: center;
-//       background-color: #9186f3;
-//       border: none;
-//       cursor: pointer;
-//       svg {
-//         color: white;
-//         font-size: 1.5rem;
-//       }
-//     }
-//   }
-// `;
 export default ChatInput;
